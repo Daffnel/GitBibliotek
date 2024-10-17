f06253fab2bc3aecc1616ffb27a30df16bbc8f4f
JennyLarsson89 <jempaz@hotmail.com>
2024-10-17 14:54:40 +0200

    whileloop påbörjad

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         Library bibliotek = new Library();
+        Scanner scanner = new Scanner(System.in);
 
+        // Förifyllda böcker
         Book bok = new Book("Kalle Hansson", "Gröna människor", 35);
         Book bok1 = new Book("Kalle Bengtsson", "Blåa människor", 35);
         Book bok2 = new Book("Bengt Karlsson", "Gula människor", 100);
         Book bok3 = new Book("Bengt Carlsson", "Röda människor", 70);
-
 
         bibliotek.addBook(bok);
         bibliotek.addBook(bok1);
         bibliotek.addBook(bok2);
         bibliotek.addBook(bok3);
 
-        bibliotek.printAllBooks();
+        while (true) {
+            System.out.println("\n1. Lägg till en bok");
+            System.out.println("2. Visa alla böcker");
+            System.out.println("3. Avsluta");
+            System.out.print("Välj ett alternativ: ");
 
-        System.out.println();
+
     }
 }
