5853e16e2eaf1e0887b87804409f6273332c3878
JennyLarsson89 <jempaz@hotmail.com>
2024-10-17 14:59:38 +0200

    whileloop färdig

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
         Library bibliotek = new Library();
         Scanner scanner = new Scanner(System.in);
 
-        // Förifyllda böcker
         Book bok = new Book("Kalle Hansson", "Gröna människor", 35);
         Book bok1 = new Book("Kalle Bengtsson", "Blåa människor", 35);
         Book bok2 = new Book("Bengt Karlsson", "Gula människor", 100);
@@ -23,6 +22,36 @@ public class Main {
             System.out.println("3. Avsluta");
             System.out.print("Välj ett alternativ: ");
 
+            int choice = scanner.nextInt();
+            scanner.nextLine();
 
+            if (choice == 1) {
+                System.out.print("Ange författare: ");
+                String author = scanner.nextLine();
+
+                System.out.print("Ange titel: ");
+                String title = scanner.nextLine();
+
+                System.out.print("Ange antal sidor: ");
+                int pages = scanner.nextInt();
+                scanner.nextLine();
+
+                Book newBook = new Book(author, title, pages);
+                bibliotek.addBook(newBook);
+                System.out.println("Boken har lagts till.\n");
+
+            } else if (choice == 2) {
+                bibliotek.printAllBooks();
+
+            } else if (choice == 3) {
+                System.out.println("Avslutar programmet.");
+                break;
+
+            } else {
+                System.out.println("Ogiltigt val, försök igen.\n");
+            }
+        }
+
+        scanner.close();
     }
 }
