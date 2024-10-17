6211568d9393b67eb9641efd4bcae877649b5bb2
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2024-10-17 15:19:27 +0200

    Library update

--- src/Library.java
+++ src/Library.java
@@ -3,9 +3,22 @@ import java.util.ArrayList;
 public class Library {
 
   private ArrayList<Book> bokLista = new ArrayList<>();
+  private ArrayList<NonFictionBook> nonFictionList = new ArrayList<>();
+  private ArrayList<FictionBook> fictionList = new ArrayList<>();
 
    public Library(){}
 
+
+
+    public void addNonFictionBook(NonFictionBook nfbook){
+
+       nonFictionList.add(nfbook);
+    }
+
+    public void addFictionBook(FictionBook fbook){
+
+       fictionList.add(fbook);
+    }
 
     public  void addBook(Book book){
 
