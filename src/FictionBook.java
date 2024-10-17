153d9dd667d9ccddec583fccd2f9258fe4915b80
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2024-10-17 14:56:57 +0200

    Second Commit Daniels_del

---
+++ src/FictionBook.java
@@ -0,0 +1,13 @@
+public class FictionBook extends Book{
+
+    private  String typeOfLiterature;
+
+    public FictionBook(String author, String title, int pages, String typeOf) {
+        super(author, title, pages);
+        this.typeOfLiterature = typeOf;
+    }
+
+
+
+
+}
