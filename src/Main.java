5a49221764a20ddf9c6e7ebe91ab9288965716e6
JennyLarsson89 <jempaz@hotmail.com>
2024-10-17 14:40:17 +0200

    Fler böcker tillagda

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,19 @@ public class Main {
 
         Library bibliotek = new Library();
 
-        Book bok = new Book("kalle Hansson","Gröna människor",35);
-        Book bok1 = new Book("kalle Bengtsson","blåa människor",35);
+        Book bok = new Book("Kalle Hansson", "Gröna människor", 35);
+        Book bok1 = new Book("Kalle Bengtsson", "Blåa människor", 35);
+        Book bok2 = new Book("Bengt Karlsson", "Gula människor", 100);
+        Book bok3 = new Book("Bengt Carlsson", "Röda människor", 70);
 
+
+        bibliotek.addBook(bok);
         bibliotek.addBook(bok1);
+        bibliotek.addBook(bok2);
+        bibliotek.addBook(bok3);
 
         bibliotek.printAllBooks();
 
-        System.out.println("Hello world!");
+        System.out.println();
     }
 }
